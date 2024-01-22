147ad6579f91d3d89272e2541ec2e7be1fb27eac
ooheunda <ooheunda@gmail.com>
2024-01-22 15:59:19 +0900

    :sparkles: add mongodb, routes connect code

--- schemas/index.js
+++ schemas/index.js
@@ -1,3 +1,19 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 dotenv.config();
+
+const connect = () => {
+  mongoose
+    .connect(process.env.DB_CONNECT_URL, {
+      dbName: "mongonara",
+    })
+    .then(() => console.log("MongoDB 연결 성공"))
+    .catch((err) => console.log(`MongoDB 연결 실패 ${err}`));
+};
+
+mongoose.connection.on("error", (err) => {
+  console.error("MongoDB 연결 에러", err);
+});
+
+export default connect;
